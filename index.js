380ef2dd4cb5f903909ce1c25fd217722cbd4e06
eriksousasp <erik@nossocrm.com.br>
2020-06-18 21:20:18 -0300

    4

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-alert('Olá boa noite!');
-window.prompt('Qual seu nome?');
-let CPF = window.prompt('Qual seu CPF?');
-if (CPF <= 99999999999) {
-    alert('Seja Bem Vindo');
-} else {
-    alert('CPF não localizado')
-}
-let Pedidos = window.prompt('Encontramos O pedido 36, o pedido 85 e o pedido 104 em seu CPF, qual deseja saber mais?');
-if (Pedidos = 36 || 85 || 104) {
-    alert('Vamos Localiza-lo, aguarde...')
-} else {
-    alert('Numero de pedido não localizado para este CPF')
-    let Pedidos = window.prompt('Por favor, digite o numero do seu pedido');
-    if (Pedidos = 36 || 85 || 104) {
-        alert('Vamos Localiza-lo, aguarde...')
+function minhaFuncao() {
+    let numero1 = document.getElementById("numero1");
+    let numero2 = document.getElementById("numero2");
+    let textoFinal = " ";
+
+    numero1 = Number(numero1);
+    numero2 = Number(numero2);
+
+    if (numero1 && numero2) {
+        if (numero1 > numero2) {
+            textoFinal = "Numero 1 é maior que numero 2";
+        } else {
+            textoFinal = "Numero 2 é maior que numero 1";
+        }
     } else {
-        alert('Pedido não localizado, por favor contate nosso suporte')
+        textoFinal = "Você precisa inserir um numero para fazer o comparativo"
     }
+    alert(textoFinal);
 }
