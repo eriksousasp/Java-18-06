092ab41b9984a48f2cc4b9d98469c05a6c06b18d
eriksousasp <erik@nossocrm.com.br>
2020-06-18 21:48:36 -0300

    5

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function minhaFuncao() {
+    alert("voce me clicou");
     let numero1 = document.getElementById("numero1");
     let numero2 = document.getElementById("numero2");
     let textoFinal = " ";
